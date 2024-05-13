a882cfeac974b28a62e18a694d74de4d5080b441
osiris <taoran0796@163.com>
2024-05-13 16:47:24 +0800

    feat: floor edit

--- src/components/Layout/Floor/Floor.vue
+++ src/components/Layout/Floor/Floor.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-const beianNo = ref('粤ICP备19038804号-1')
-const copyright = ref('ranblogs.com')
+import {  ref } from 'vue';
+const appDesc = ref(import.meta.env.VITE_APP_DESC || '')
 </script>
 
 <template>
   <div class="bg-white w-full rounded-2xl z-10 p-5 text-center">
-    Copyright © 2023  · {{ copyright }} &nbsp;&nbsp; <a href="https://xy.ranblogs.com/tools_web.html" target="_blank">隐私权政策</a> &nbsp;&nbsp; <a href="https://beian.miit.gov.cn/" target="_blank">{{ beianNo }}</a> 
+    {{ appDesc }}  © 2024 <a href="https://github.com/taoran1401/tools-web" target="_blank" class="text-blue-700">Tools-Web</a>
   </div>
 </template>
 
