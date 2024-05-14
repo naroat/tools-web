efc1cb52eba3222d89fbe7ec9dcbcca4b2631fee
osiris <taoran0796@163.com>
2024-05-14 11:00:42 +0800

    style

--- src/components/Layout/Floor/Floor.vue
+++ src/components/Layout/Floor/Floor.vue
@@ -4,7 +4,7 @@ const appDesc = ref(import.meta.env.VITE_APP_DESC || '')
 </script>
 
 <template>
-  <div class="bg-white w-full rounded-2xl z-10 p-5 text-center">
+  <div class="w-full rounded-2xl z-10 p-5 text-center">
     {{ appDesc }}  © 2024 <a href="https://github.com/taoran1401/tools-web" target="_blank" class="text-blue-700">Tools-Web</a>
   </div>
 </template>
